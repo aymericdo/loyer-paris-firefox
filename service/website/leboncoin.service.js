c376b81c15fa19cf33b3b4a026deec3d8c977686
aymeric dominique <aymeric.dominique@gmail.com>
2023-08-25 18:06:52 +0200

    6.0.9

--- service/website/leboncoin.service.js
+++ service/website/leboncoin.service.js
@@ -1,4 +1,9 @@
 class LeboncoinWebsite extends WebsiteService {
+  constructor() {
+    super();
+    this.fireKeyword = '#map > div > h2';
+  }
+
   getId() {
     const url = window.location.toString()
     const match = url.match(/(?<=\/locations\/)\d+(?=.htm)/g) || url.match(/(?<=\/vi\/)\d+(?=.htm)/g)
--- service/website/pap.service.js
+++ service/website/pap.service.js
@@ -2,11 +2,7 @@ class PapWebsite extends WebsiteService {
   getId() {
     const url = window.location.toString();
     const match = url.match(/(?<=annonces\/appartement(.)+)(?<=-)r\d+/g);
-    const sectionName = document.querySelector(
-      "[itemprop=itemListElement] > a[itemprop=item] > span[itemprop=name]"
-    );
-    return match && sectionName.textContent.trim().includes("Location")
-      ? match[0]
-      : null;
+
+    return match ? match[0] : null;
   }
 }
