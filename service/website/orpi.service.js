45edd708b91c866a6a780d36a638cf57bbcd3d57
aymeric dominique <aymeric.dominique@gmail.com>
2024-01-27 16:37:31 +0100

    orpi

--- service/website/orpi.service.js
+++ service/website/orpi.service.js
@@ -1,7 +1,7 @@
 class OrpiWebsite extends WebsiteService {
   getId() {
     const url = window.location.toString();
-    const match = url.match(/(?<=annonce-location-appartement.*)b-.*(?=\/)/g);
+    const match = url.match(/(?<=annonce-location-appartement.*).*(?=\/)/g);
     return match ? match[0] : null;
   }
 }
